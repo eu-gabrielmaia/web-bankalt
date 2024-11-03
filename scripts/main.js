9f7f20bd9124a654821166f13585134db1e81b99
Gabriel Maia <gabrielmaia7g@gmail.com>
2024-11-02 22:08:58 -0300

    melhorando navbar mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,23 @@
-const btnMobileLista = document.querySelector('.btnMenu')
+const btnMobileLista = document.querySelector('.btnMenu');
+const iconMobileLista = document.querySelector('.btnMenu img');
 const navMobileLista = document.querySelector('.drop-down-mobile');
+const menuNavMobile = document.querySelector('.nav-lista-mobile');
+const checkboxBurger = document.querySelector("#burger");
 
 btnMobileLista.addEventListener('click', ()=> {
+    if(navMobileLista.classList.contains('btnMenu-hidden')){
+        iconMobileLista.style.transform = "rotate(0deg)";
+    }
+    else{
+        iconMobileLista.style.transform = "rotate(180deg)";
+    }
     navMobileLista.classList.toggle('btnMenu-hidden');
+})
+
+checkboxBurger.addEventListener('change', () => {
+    if (checkboxBurger.checked) {
+        menuNavMobile.style.display = "block";
+    } else {
+        menuNavMobile.style.display = "none";
+    }
 })
